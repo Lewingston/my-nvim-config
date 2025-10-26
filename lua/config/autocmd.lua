564c048038728bb94433176fc53a8185a161b640
Lewingston <jonas.speyer@web.de>
2025-10-26 16:34:09 +0100

    Fixed typo

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -3,7 +3,7 @@
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
     pattern = "*",
     callback = function(args)
-        if vim.bo[args.buf].buftype == "" then -- Only show line number is buffer is actualy a file
+        if vim.bo[args.buf].buftype == "" then -- Only show line number if buffer is actualy a file
             vim.api.nvim_command(":setlocal relativenumber")
         end
     end
