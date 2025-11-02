5be3020a90224338bf829fffdb2294092733594d
Lewingston <jonas.speyer@web.de>
2025-11-02 10:48:53 +0100

    Added telescope plugin and config

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,35 @@
+
+return {
+
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+
+    config = function()
+
+        local builtin = require('telescope.builtin')
+
+        vim.keymap.set("n", "<leader>ff", builtin.find_files)
+
+        vim.keymap.set("n", "<leader>fc", function()
+            builtin.find_files {
+                cwd = vim.fn.stdpath("config")
+            }
+        end)
+
+        vim.keymap.set("n", "<leader>fb", builtin.buffers)
+
+        vim.keymap.set("n", "<leader>fr", builtin.lsp_references)
+
+        vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols)
+
+        vim.keymap.set("n", "<leader>fd", builtin.diagnostics)
+
+        vim.keymap.set("n", "<leader>F", builtin.live_grep)
+
+        vim.keymap.set("n", "<leader>fF", builtin.grep_string)
+
+        vim.keymap.set("n", "<leader>fi", builtin.current_buffer_fuzzy_find)
+
+    end
+}
