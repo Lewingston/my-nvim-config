2b1b506a8119eaed2032c2b0798eea8924abf0db
Lewingston <jonas.speyer@web.de>
2025-11-13 20:14:13 +0100

    Added shortcut to jump to function definition

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,9 +1,14 @@
 
 vim.lsp.config['cpp'] = {
     cmd = { 'C:/msys64/mingw64/bin/clangd.exe',
-            '--compile-commands-dir=./build/' },
+            '--compile-commands-dir=./build/',
+            '--background-index'},
     filetypes = { 'cpp', 'hpp', 'c', 'h' },
     root_markers = { { 'CMakeLists.txe' } }
 }
 
 vim.lsp.enable('cpp')
+
+vim.keymap.set('n', 'grd', function()
+    vim.lsp.buf.definition()
+end)
